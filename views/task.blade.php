5044a185a555f514aabf22c61fe5128ce2d152b8
sereenmethgal <102178221+sereenmethgal@users.noreply.github.com>
2022-04-08 18:01:54 +0300

    Update task.blade.php

--- views/task.blade.php
+++ views/task.blade.php
@@ -1,8 +1,14 @@
-
 <!DOCTYPE html>
 <html>
 <body>
 
-<h1>{{$task}} </h1>
+
+
+
+    <h1> {{$task->name}} </h1>
+
+    <h1> {{$task->id}} </h1>
+    <h1> {{$task->created_at}} </h1>
+
 </body>
 </html>
