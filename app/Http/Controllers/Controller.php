dfd54d8efe4c351dabdddbe5844c83dd00f7ffb0
sereenmethgal <102178221+sereenmethgal@users.noreply.github.com>
2022-04-08 17:53:59 +0300

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-
-class Controller extends BaseController
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\support\Facades\DB;
+class ProductsController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function index(){
+
+       // $tasks =[
+
+          //  'task 1'=>'Make your assignment',
+           // 'task 2' => 'check your phone' ,
+           // 'task 3'=>'watch a match'
+       // ];
+        $tasks = DB:: table('products')->where('name','like','product%')->get();
+        return view('dbtask' , compact('tasks')) ;
+
+    }
+
+    public function show($id){
+
+        $task = DB:: table('products')->find($id);
+        return view('dbtask1' , compact('task')) ;
+
+    }
+    public function stor(){
+
+        $task = DB:: table('products')-> insert(['name'=>$_POST['name']]);
+        return redirect() -> back() ;
+
+    }
+
+    // public function send_name(){
+    //     $name = $_REQUEST['name'];
+    //     return view('dbtask',compact('name'));
+    // }
+
+    // public function show_name(){
+    //     $name = 'sereen';
+    //     $age = 22;
+    //     return view('dbtask',compact('name','age'));
+    // }
 }
