0aba2420bdf93f76ed04de2ca8c43a6671622df0
sereenmethgal <102178221+sereenmethgal@users.noreply.github.com>
2022-04-18 12:04:44 +0300

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,43 +6,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\support\Facades\DB;
-class ProductsController extends Controller
+
+class Controller extends BaseController
 {
-    public function index(){
-
-       // $tasks =[
-
-          //  'task 1'=>'Make your assignment',
-           // 'task 2' => 'check your phone' ,
-           // 'task 3'=>'watch a match'
-       // ];
-        $tasks = DB:: table('products')->where('name','like','product%')->get();
-        return view('dbtask' , compact('tasks')) ;
-
-    }
-
-    public function show($id){
-
-        $task = DB:: table('products')->find($id);
-        return view('dbtask1' , compact('task')) ;
-
-    }
-    public function stor(){
-
-        $task = DB:: table('products')-> insert(['name'=>$_POST['name']]);
-        return redirect() -> back() ;
-
-    }
-
-    // public function send_name(){
-    //     $name = $_REQUEST['name'];
-    //     return view('dbtask',compact('name'));
-    // }
-
-    // public function show_name(){
-    //     $name = 'sereen';
-    //     $age = 22;
-    //     return view('dbtask',compact('name','age'));
-    // }
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
