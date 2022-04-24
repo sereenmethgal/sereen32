712da9602447be3e7ed2096bc50f35f33cfddd18
sereenmethgal <102178221+sereenmethgal@users.noreply.github.com>
2022-04-24 21:36:57 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,46 @@ Route::get('/show',[PostController::class,'show']);
 Route::get('/delete/{id}',[PostController::class,'destroy']);
 Route::get('/edit/{id}',[PostController::class,'edit']);
 Route::post('/update/{id}',[PostController::class,'update']);
+}
+Route::get('tasks',function(){
+}};
+           $task=DB::table('tasks')->get();
+           //dd($tasks);
+           return view ('tasks',compact('tasks'));
+           }};
+           Route::get('tasks/show/{id}', function ($id) {
+          $task=DB::table('tasks')->find($id);
+           //dd($tasks);
+           return view ('show',compact('task'));
+           }};
+            Route::get('app',function(){
+                //$tasks =DB::table('tasks')->get();
+                $tasks =Task::all();
+                dd( $tasks);
+             return view ('todo',compact('tasks'));
+            }};
+                       
+      Route::post('store', function (Request $request) {
+ //DB::table('tasks')->insert({
+    // 'title'=>$request->title ;
+          $task =new Task;
+       $task-> title = $request->title ;
+$task->save();
+         return redirect()->back();
+          
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+
+            
+          
+
+
